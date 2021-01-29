d8defc07abff4335a88fd97f3a66dc883a1be5e0
Kovács Dorottya <kovacsdorika@gmail.com>
2021-01-29 22:53:29 +0100

    Default classes constructors.

--- src/app/model/category.ts
+++ src/app/model/category.ts
@@ -2,4 +2,13 @@ export class Category {
     id: number;
     name: string;
     description: string;
+
+    constructor(
+        id: number, 
+        name: string, 
+        description: string) {
+            this.id = id || 0;
+            this.name = name || "";
+            this.description = description || "";
+    }
 }
--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -8,4 +8,26 @@ export class Product {
     stock: number;
     featured: boolean;
     active: boolean;
+
+    constructor(
+        id: number,
+        catId: number,
+        name: string,
+        description: string,
+        image: string,
+        price: number,
+        stock: number,
+        featured: boolean,
+        active: boolean
+        ) {
+            this.id = id || 0;
+            this.catId = catId || 0;
+            this.name = name || "";
+            this.description = description || "";
+            this.image = image || "";
+            this.price = price || 0;
+            this.stock = stock || 0;
+            this.featured = featured || false;
+            this.active = active || true;
+    }
 }
