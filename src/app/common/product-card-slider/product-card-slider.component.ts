bff7a907af20153c344f1faa423b418329e893d8
Kovács Dorottya <kovacsdorika@gmail.com>
2021-01-30 17:35:40 +0100

    Product card slider, home page.

---
+++ src/app/common/product-card-slider/product-card-slider.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-slider',
+  templateUrl: './product-card-slider.component.html',
+  styleUrls: ['./product-card-slider.component.scss']
+})
+export class ProductCardSliderComponent implements OnInit {
+
+  products: string[] = ["alma", "körte", "barack", "citrom", "banán"]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
